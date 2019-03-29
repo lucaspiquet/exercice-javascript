8f2024c430ca930a9349db115a1b272443e6e5a6
lucaspiquet <48794360+lucaspiquet@users.noreply.github.com>
2019-03-29 23:56:24 +0100

    Code non fonctionnel : liste des méthodes

---
+++ scripts/main.js
@@ -0,0 +1,35 @@
+document.querySelector('html').addEventListener('onload', function() { /* Evénement onload = le navigateur a fini de charger le code HTML */
+setTimeout(function, 10000); /* Exécute le script 10 000 millisecondes après le chargement du code HTML */
+window.confirm("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please"); /* Confirm box : pop-up où l'utilisateur doit cliquer sur OK ou Annuler */
+});
+
+
+
+let myButtonOK = document.querySelector('OK');
+
+myImage.addEventListener('click', function() {
+    let mySrc = monImage.getAttribute('src');
+    if (myAnswer === 'OK') { /* Si OK */
+      myImage.setAttribute('src', 'images/firefox2.png'); /* Alors redirection vers YouTube */
+    } /* Pas forcément besoin de "else" */
+});
+
+
+
+
+let myButtonMode = document.querySelector('button'); /* Référénce au bouton ajouté dans le code HTML */
+
+myButton.addEventListener('click', function() {
+  setUserName();
+});
+
+background-color: #FF00FF
+
+h1, h2, h3
+color: #008000
+
+p
+font-family: papyrus
+
+header
+font-family: Comic Sans
